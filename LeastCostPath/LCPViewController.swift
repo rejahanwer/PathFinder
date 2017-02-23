ec330d0875f25362e4bb720529d30d01ce0ae3ba
Anuvar, Rejah <rejahanwer@gmail.com>
2017-02-23 23:51:19 +0530

    Configuration changes.

---
+++ LeastCostPath/LCPViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  LeastCostPath
+//
+//  Created by Anuvar, Rejah on 2/23/17.
+//  Copyright © 2017 rejah. All rights reserved.
+//
+
+import UIKit
+
+class LCPViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // Check if in valid format.
+    func isValidInput(input: String) -> Bool {
+        // Check if \n and spaces are correct.
+        // Check if all numeric values
+        //
+        
+        return false
+    }
+
+    func hasNonNumerics(string: String) -> Bool {
+        return false
+    }
+    
+    func isEmptyString(string: String) -> Bool {
+        return false
+    }
+    
+    func validateRowSize(input: String) -> Bool {
+        return false
+    }
+    
+    func validateColumnSize(input: String) -> Bool {
+        return false
+    }
+}
