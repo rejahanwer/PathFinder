4de3003c3e92bcd99a2dd4cfa6d740de71bd2fa2
Anuvar, Rejah <rejahanwer@gmail.com>
2017-02-27 00:29:58 +0530

    Code cleanup.

--- LeastCostPathTests/LCPInputTests.swift
+++ LeastCostPathTests/LCPInputTests.swift
@@ -14,12 +14,10 @@ class LCPInputTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
         viewController = LCPViewController()
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
