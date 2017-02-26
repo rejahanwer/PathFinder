e716ef80470d4dd322da215770f066d3d863622e
Anuvar, Rejah <rejahanwer@gmail.com>
2017-02-26 23:18:53 +0530

    Added UI.

--- LeastCostPath/LCPViewController.swift
+++ LeastCostPath/LCPViewController.swift
@@ -9,7 +9,43 @@
 import UIKit
 
 class LCPViewController: UIViewController {
+    
+    var pathManager = LCPManager()
+    @IBOutlet weak var inputTextView: UITextView!
+    @IBOutlet weak var outputTextView: UITextView!
+    @IBOutlet weak var computeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    @IBAction func computeButtonAction() {
+        if let inputString = inputTextView.text {
+            let result = pathManager.getLowestCostPath(inputString: inputString)
+            
+            var output1 = ""
+            var output2 = 0
+            var output3: [Int] = []
+            
+            switch result.result {
+            case .invalid:
+                output1 = "Invalid input"
+                output2 = result.cost
+                output3 = result.path
+                outputTextView.text = "\(output1)"
+            case .yes:
+                output1 = "Yes"
+                output2 = result.cost
+                output3 = result.path
+
+                outputTextView.text = "\(output1)\n\(output2)\n\(output3)"
+            case .no:
+                output1 = "No"
+                output2 = result.cost
+                output3 = result.path
+
+                outputTextView.text = "\(output1)\n\(output2)\n\(output3)"
+            }
+        }
     }
 }
